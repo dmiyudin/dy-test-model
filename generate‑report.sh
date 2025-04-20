8e132ff1fcbdc34d6b79911dc9ba181524d8fe7c
DmiYudin <dyudin@mail.ru>
2025-04-20 23:10:21 +0300

    Create generate‑report.sh

---
+++ generate‑report.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Путь к исполняемому файлу Archi 
+ARCHI_EXECUTABLE="C:\Program Files\Archi\Archi.exe"
+
+# Путь к модели
+MODEL_FILE="model/SS-Upstream.archimate"
+
+# Папка для HTML-отчета
+REPORT_DIR="html-reports/SS-Upstream"
+
+# Генерация отчета
+$ARCHI_EXECUTABLE -application com.archimatetool.commandline.app \
+  -consoleLog -nosplash \
+  -loadModel "$MODEL_FILE" \
+  -html.createReport "$REPORT_DIR"
+
+# Добавление отчета в git
+git add "$REPORT_DIR"
+git commit -m "Автоматическая генерация HTML-отчета"
